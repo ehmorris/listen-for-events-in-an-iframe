6813521e7c6db8c0c859cfb3c343f763696e61b1
Edwin Morris <edwin@rapgenius.com>
2015-05-19 20:06:38 -0400

    first stab at listening for events inside iframes
    and reacting from the parent frame
    
    need to:
    * add a unique id to each event

---
+++ listen-for-events-in-an-iframe.js
@@ -0,0 +1,19 @@
+var frame_on = function(channel, event, selector, func) {
+  channel.notify({
+    method: "listen_for_event",
+    params: {
+      event: event,
+      selector: selector
+    }
+  })
+
+  channel.bind("react_to_event", func)
+}
+
+var transmit_events_to = function(channel) {
+  channel.bind("listen_for_event", function(_, params) {
+    document.querySelector(params.selector).addEventListener(params.event, function() {
+      channel.notify({ method: "react_to_event" })
+    })
+  })
+}
