3284b59e03ae1c0c638732aaae034ac9cb3d2600
Edwin Morris <edwin@rapgenius.com>
2015-05-29 19:39:12 -0400

    add semicolons, use classes, use on method
    with classes we can actually construct event listeners the same way
    they're constructed normally. cool!

--- listen-for-events-in-an-iframe.js
+++ listen-for-events-in-an-iframe.js
@@ -1,19 +1,23 @@
-var frame_on = function(channel, event, selector, func) {
-  channel.notify({
-    method: "listen_for_event",
-    params: {
-      event: event,
-      selector: selector
-    }
-  })
+function ListenForIFrameEventsFrom(channel) {
+  this.channel = channel;
 
-  channel.bind("react_to_event_"+event+selector, func)
-}
+  this.on = function(event, selector, func) {
+    this.channel.notify({
+      method: "listen_for_event",
+      params: {
+        event: event,
+        selector: selector
+      }
+    });
 
-var transmit_events_to = function(channel) {
+    this.channel.bind("react_to_event_"+event+selector, func);
+  };
+};
+
+function TransmitEventsTo(channel) {
   channel.bind("listen_for_event", function(_, params) {
     document.querySelector(params.selector).addEventListener(params.event, function() {
-      channel.notify({ method: "react_to_event_"+params.event+params.selector })
-    })
-  })
-}
+      channel.notify({ method: "react_to_event_"+params.event+params.selector });
+    });
+  });
+};
