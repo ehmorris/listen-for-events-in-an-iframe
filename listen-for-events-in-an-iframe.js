4f1f6134556eb582c0b85d38cd7b6ee23efb3fb5
Edwin Morris <edwin@rapgenius.com>
2015-06-15 20:09:02 -0400

    pass event object out of iframe, update demo
    previously the event object was returning nill b/c you can't pass
    circular references like `target` out of one DOM and into another. added
    a method that strips out as many properties as we can transmit (may be
    missing some) and sends those instead of the raw event object.
    
    updated the demo to play with the mouse x/y coordinates

--- listen-for-events-in-an-iframe.js
+++ listen-for-events-in-an-iframe.js
@@ -1,3 +1,52 @@
+var transmitable_event_object_properties = [
+  'altKey',
+  'bubbles',
+  'button',
+  'buttons',
+  'cancelBubble',
+  'cancelable',
+  'charCode',
+  'clientX',
+  'clientY',
+  'ctrlKey',
+  'dataTransfer',
+  'defaultPrevented',
+  'detail',
+  'eventPhase',
+  'keyCode',
+  'layerX',
+  'layerY',
+  'loaded',
+  'metaKey',
+  'movementX',
+  'movementY',
+  'offsetX',
+  'offsetY',
+  'pageX',
+  'pageY',
+  'returnValue',
+  'screenX',
+  'screenY',
+  'shiftKey',
+  'timeStamp',
+  'type',
+  'webkitMovementX',
+  'webkitMovementY',
+  'which',
+  'x',
+  'y'
+];
+
+var get_transmitable_event_object = function(event_object) {
+  var new_object = {};
+
+  transmitable_event_object_properties.forEach(function(property, _, __) {
+    new_object[property] = event_object[property];
+  });
+
+  return new_object;
+};
+
 function ListenForIFrameEventsFrom(iframe_element) {
   this.channel = Channel.build({
     window: iframe_element.contentWindow,
@@ -14,7 +63,9 @@ function ListenForIFrameEventsFrom(iframe_element) {
       }
     });
 
-    this.channel.bind("react_to_event_"+event+"_"+selector, func);
+    this.channel.bind("react_to_event_"+event+"_"+selector, function(_, params) {
+      func(params);
+    });
   };
 }
 
@@ -26,8 +77,12 @@ function TransmitEventsToParent() {
   });
 
   channel.bind("listen_for_event", function(_, params) {
-    document.querySelector(params.selector).addEventListener(params.event, function() {
-      channel.notify({ method: "react_to_event_"+params.event+"_"+params.selector });
+    document.querySelector(params.selector).addEventListener(params.event, function(event_object) {
+      var transmitable_event_object = get_transmitable_event_object(event_object);
+      channel.notify({
+        method: "react_to_event_"+params.event+"_"+params.selector,
+        params: transmitable_event_object
+      });
     });
   });
 }
